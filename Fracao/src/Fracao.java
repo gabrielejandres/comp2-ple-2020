f66fd95e427da9e10f0ad7de406d53be1368edd4
Gabriele Jandres <gabrielejandres@gmail.com>
2020-09-11 12:27:12 -0300

    Testes passando

--- Fracao/src/Fracao.java
+++ Fracao/src/Fracao.java
@@ -1,5 +1,9 @@
 public class Fracao {
 
+    private int numerador;
+    private int denominador;
+    private boolean positiva;
+
     /**
      * Construtor.
      * O sinal da fração é passado no parâmetro específico.
@@ -9,14 +13,35 @@ public class Fracao {
      * @param positiva se true, a fração será positiva; caso contrário, será negativa
      */
     public Fracao(int numerador, int denominador, boolean positiva) {
-        // ToDo IMPLEMENT ME!!!
+        this.numerador = numerador;
+        this.denominador = denominador;
+        this.positiva = positiva;
+    }
+
+    // Getters and setters
+    public int getNumerador() {
+        return this.numerador;
+    }
+
+    public int getDenominador() {
+        return this.denominador;
+    }
+
+    public boolean isPositiva() {
+        if(this.numerador == 0)
+            return false;
+        return this.positiva;
     }
 
     /**
      * @return um double com o valor numérico desta fração
      */
     public double getValorNumerico() {
-        return 0;  // ToDo IMPLEMENT ME!!!
+        if (this.numerador == 0) {
+            return 0;
+        }
+        int mult = this.isPositiva() ? 1 : -1;
+        return (mult * ((double) this.numerador / (double) this.denominador));
     }
 
     /**
@@ -28,18 +53,40 @@ public class Fracao {
      *         no caso desta fração JÁ SER ela própria irredutível, retorna this
      */
     public Fracao getFracaoGeratriz() {
-        return null;  // ToDo IMPLEMENT ME!!!
+        int mdc = mdc(this.numerador, this.denominador);
+        if (mdc == 1) {
+            return this;
+        }
+        int novoNumerador = this.numerador / mdc;
+        int novoDenominador = this.denominador / mdc;
+        return new Fracao(novoNumerador, novoDenominador, this.positiva);
     }
 
-    public int getNumerador() {
-        return 0;  // ToDo IMPLEMENT ME!!!
+    /**
+     *
+     * @param dividendo O número que será dividido (maior número)
+     * @param divisor O número pelo qual o dividendo será dividido
+     * @return maior divisor comum entre dividendo e divisor
+     */
+    private int mdc(int dividendo, int divisor) {
+        return dividendo % divisor == 0 ? divisor : mdc(divisor, dividendo % divisor);
     }
 
-    public int getDenominador() {
-        return 0;  // ToDo IMPLEMENT ME!!!
-    }
+    /**
+     *
+     * @param o Um objeto, que deverá ser uma conta
+     * @return um booleano que indica se as frações são equivalentes
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Fracao novaFracao = (Fracao) o;
 
-    public boolean isPositiva() {
-        return false;  // ToDo IMPLEMENT ME!!!
+        boolean mesmoNumerador = novaFracao.getFracaoGeratriz().getNumerador() == this.getFracaoGeratriz().getNumerador();
+        boolean mesmoDenominador = novaFracao.getFracaoGeratriz().getDenominador() == this.getFracaoGeratriz().getDenominador();
+        boolean mesmoSinal = novaFracao.getFracaoGeratriz().isPositiva() == this.getFracaoGeratriz().isPositiva();
+
+        return mesmoNumerador && mesmoDenominador && mesmoSinal;
     }
 }
